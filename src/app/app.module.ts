6b6cce84d2d66c214e528fd8970ac079fc812ef5
selamshimelis <shimeselam@gmail.com>
2020-10-23 16:14:06 +0300

    sign-in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,35 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//import { AuthService } from './shared/services/auth.service';
+
+// Firebase
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    //AuthService,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'my-app-name'),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBItjeutRJvOx72hFdmh7XeGZe4zAZJ6ow",
+    authDomain: "angularexam-77006.firebaseapp.com",
+    databaseURL: "https://angularexam-77006.firebaseio.com",
+    projectId: "angularexam-77006",
+    storageBucket: "angularexam-77006.appspot.com",
+    messagingSenderId: "677773787452",
+    appId: "1:677773787452:web:5e75fd54c33d82de555e39",
+    measurementId: "G-8SBVCV9647"
+  }
 };
